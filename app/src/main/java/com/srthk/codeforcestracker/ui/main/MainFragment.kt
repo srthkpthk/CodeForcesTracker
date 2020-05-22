9564e5dbe7d9024b0deb131d93ebbd7875c57fc1
Srthk <techysrthk@gmail.com>
2020-05-22 17:38:48 +0530

    added spinner

--- app/src/main/java/com/srthk/codeforcestracker/ui/main/MainFragment.kt
+++ app/src/main/java/com/srthk/codeforcestracker/ui/main/MainFragment.kt
@@ -2,6 +2,9 @@ package com.srthk.codeforcestracker.ui.main
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,32 +22,68 @@ import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
 
-class MainFragment : Fragment(R.layout.main_fragment), KodeinAware {
+class MainFragment : Fragment(R.layout.main_fragment), KodeinAware,
+    AdapterView.OnItemSelectedListener {
     private lateinit var viewModel: MainViewModel
     private val factory: MainViewModelFactory by instance()
     override val kodein by kodein()
+    private val contestList = mutableListOf<AvailableContests>()
+    private var filterOptions = arrayOf("ALL", "BEFORE", "FINISHED")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
+        showLoading(true)
         getData()
+    }
+
+    private fun showLoading(flag: Boolean) {
+        spinner2.isVisible = !flag
+        shimmer.isVisible = flag
+        if (flag) {
+            shimmer.stopShimmer()
+        } else {
+            shimmer.stopShimmer()
+        }
+    }
+
+    private fun setupSpinner() {
+        val aa = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, filterOptions)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.apply {
+            adapter = aa
+            setSelection(0, false)
+            onItemSelectedListener = this@MainFragment
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        showRecyclerView(if (position == 0) {
+            contestList.toRecyclerViewItem()
+        } else {
+            contestList.filter { it.phase == filterOptions[position] }
+                .toRecyclerViewItem()
+        })
     }
 
     private fun getData() = CoroutineScope(Dispatchers.Main).launch {
         viewModel.availableContests.await().observe(viewLifecycleOwner, Observer {
             when {
                 viewModel.isInternetAvailable && !viewModel.isApiException -> {
-                    println(it)
                     when {
                         !it.isNullOrEmpty() -> {
-                            showRecyclerView(it.filter { it.phase != "FINISHED" }
-                                .toRecyclerViewItem())
+                            contestList.addAll(it)
+                            setupSpinner()
                         }
                     }
                 }
                 else -> {
                     this@MainFragment.requireContext()
                         .toast("There's an error better check for internet")
-//                    showLoading(false)
+                    showLoading(false)
                 }
             }
         })
@@ -58,9 +97,8 @@ class MainFragment : Fragment(R.layout.main_fragment), KodeinAware {
             layoutManager = LinearLayoutManager(context)
             adapter = grpAdapter
         }
+        showLoading(false)
     }
-
-
 }
 
 private fun List<AvailableContests>.toRecyclerViewItem() =
